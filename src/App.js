23dd4aacfc58d8607c4d6b2fbbd354c7d9a4c85d
German Hornus <hornusgerman@gmail.com>
2021-12-05 23:57:04 -0300

    Video2-min52:00
    Comit seteando el usuario

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 import getGifs from './services/getGifs';
+import Gif from './components/Gif'
 
 
 function App() {
   const [gifs, setGifs] = useState([])
 
   useEffect(function (){
-    getGifs({keyword: 'rick'}).then(gifs => setGifs(gifs))
+    getGifs({keyword: 'JavaScript'}).then(gifs => setGifs(gifs))
   },[])
 
   return (
     <div className="App">
       <section className="App-content">
       {
-        gifs.map(singleGif =>{
-          return<div>
-          <h4>{singleGif.title}</h4>
-          <small>{singleGif.id}</small>
-          <img alt={singleGif.title} src={singleGif.url}/>
-          </div>
-          })
+        gifs.map(singleGif =>
+        <Gif 
+        key = {singleGif.id}
+        title={singleGif.title} 
+        url={singleGif.url} 
+        id={singleGif.id}
+
+        />)
       }
       </section>
     </div>
